49ae5a4e96aba4d7be51b6cbad46de1e724c5099
FILIPPOS-ATHANASOPOULOS-ANTYRAS <filipposfivos@gmail.com>
2023-10-11 17:31:44 +0300

    test feature branch

--- soft-devII-2024-project-material/sales-commissions-application/src/gui/InputWindow.java
+++ soft-devII-2024-project-material/sales-commissions-application/src/gui/InputWindow.java
@@ -52,6 +52,7 @@ public class InputWindow extends JDialog {
 	/**
 	 * Launch the application.
 	 */
+	//commit from new feature branch
 	public static void main(String[] args) {
 		try {
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
