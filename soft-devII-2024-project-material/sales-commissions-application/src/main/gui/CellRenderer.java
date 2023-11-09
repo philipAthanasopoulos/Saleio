db66090586d429408547835c8ee2ce246fcf16b5
philipAthanasopoulos <filipposfivos@gmail.com>
2023-11-09 21:15:59 +0200

    Remove unused imports and code in CellRenderer.java

--- soft-devII-2024-project-material/sales-commissions-application/src/main/gui/CellRenderer.java
+++ soft-devII-2024-project-material/sales-commissions-application/src/main/gui/CellRenderer.java
@@ -9,9 +9,7 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 import javax.swing.BorderFactory;
 import java.awt.BorderLayout;
-import javax.swing.JButton;
 import java.awt.Color;
-import java.awt.Cursor;
 
 
 public class CellRenderer extends DefaultListCellRenderer {
