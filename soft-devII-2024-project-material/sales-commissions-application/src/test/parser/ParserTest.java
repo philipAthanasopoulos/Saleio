3095bab7e3e6db71972de8ef2802070bfa6ba722
philipAthanasopoulos <filipposfivos@gmail.com>
2023-11-23 13:29:42 +0200

    Fix file path issue in ParserTest.java

--- soft-devII-2024-project-material/sales-commissions-application/src/test/parser/ParserTest.java
+++ soft-devII-2024-project-material/sales-commissions-application/src/test/parser/ParserTest.java
@@ -22,8 +22,6 @@ public class ParserTest {
 	File testCase2XML;
 	File testCase1HTML;
 	File testCase2HTML;
-	
-	String sourceFolder = getClass().getResource("/resources/test_input_files/").getFile();
 
 	@Before
 	public void setUp() {
@@ -40,7 +38,7 @@ public class ParserTest {
 		testCase2XML = new File(getClass().getResource("/resources/test_input_files/test-case-2-XML.xml").getFile());
 
 		testCase1HTML = new File(getClass().getResource("/resources/test_input_files/test-case-1-HTML.html").getFile());
-		// testCase2HTML = new File(getClass().getResource("/resources/test_input_files/test-case-2-HTML.html").getFile());
+		testCase2HTML = new File(getClass().getResource("/resources/test_input_files/test-case-2-HTML.html").getFile());
 	}
 	
 	@Test
@@ -52,24 +50,28 @@ public class ParserTest {
 			Associate associate3 = parserXML.parseAssociateFromFile(testCase1XML);
 			Associate associate4 = parserXML.parseAssociateFromFile(testCase2XML);
 			Associate associate5 = parserHTML.parseAssociateFromFile(testCase1HTML);
+			Associate associate6 = parserHTML.parseAssociateFromFile(testCase2HTML);
 
 			assertEquals("Apostolos Zarras", associate1.getName());
 			assertEquals("Dimitris Papadopoulos", associate2.getName());
 			assertEquals("Iosif Emmanouilidis", associate3.getName());
 			assertEquals("Vassileios Zarras", associate4.getName());
 			assertEquals("Apostolos Zarras", associate5.getName());
+			assertEquals("George Johnson", associate6.getName());
 
 			assertEquals(associate1.getReceipts().size(), 18);
 			assertEquals(associate2.getReceipts().size(), 4);
 			assertEquals(associate3.getReceipts().size(), 4);
 			assertEquals(associate4.getReceipts().size(), 16);
 			assertEquals(associate5.getReceipts().size(), 4);
+			assertEquals(associate6.getReceipts().size(), 4);
 
 			assertEquals(associate1.getPersonalFile(), testCase1TXT);
 			assertEquals(associate2.getPersonalFile(), testCase2TXT);
 			assertEquals(associate3.getPersonalFile(), testCase1XML);
 			assertEquals(associate4.getPersonalFile(), testCase2XML);
 			assertEquals(associate5.getPersonalFile(), testCase1HTML);
+			assertEquals(associate6.getPersonalFile(), testCase2HTML);
 
 
 		} catch (Exception e) {
